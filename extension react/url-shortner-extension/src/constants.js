ecd7f0d79987b56e030206209d1231f38dd3fcde
Siddharth Sharma <vsssiddharth@gmail.com>
2024-08-11 14:45:25 +0530

    update constants.js

--- extension react/url-shortner-extension/src/constants.js
+++ extension react/url-shortner-extension/src/constants.js
@@ -1,3 +1,3 @@
 export const frontendURL = "http://localhost:5173";
-// export const backendURL = "https://url-shortner-api-zeta.vercel.app";
-export const backendURL = "http://localhost:3000";
+export const backendURL = "https://url-shortner-api-zeta.vercel.app";
+// export const backendURL = "http://localhost:3000";
